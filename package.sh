16e95351304ade8f7901d9573950ab11529e4760
Joe <joe@iachieved.it>
2015-12-12 15:31:57 -0600

    Correction of install path

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 pushd `dirname $0` > /dev/null
 WHERE_I_AM=`pwd`
 popd > /dev/null
-INSTALL_DIR$=${WHERE_I_AM}/install
+INSTALL_DIR=${WHERE_I_AM}/install
 PACKAGE=${WHERE_I_AM}/swift.tar.gz
 rm -rf $INSTALL_DIR $PACKAGE
 ./swift/utils/build-script --preset=buildbot_linux_1404 install_destdir=${INSTALL_DIR} installable_package=${PACKAGE}
