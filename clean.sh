412fa44c73ab5f09b365b29d9028f1fde0100a3d
Joe <joe@iachieved.it>
2015-12-12 12:50:24 -0600

    Ask before deleting build

--- clean.sh
+++ clean.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-rm -rf build
+#!/bin/bash
+read -r -p "Are you sure you want to delete the build directory? [y/N] " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    echo "Deleting build artifacts"
+    rm -rf build
+    echo "Done"
+else
+    echo "Nothing deleted"
+fi
