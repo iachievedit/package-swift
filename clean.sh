50537c69dac61e97119e0c8a9aa1c5639d5e33da
Joe <joe@iachieved.it>
2015-12-12 13:01:06 -0600

    Install package to install in working area

--- clean.sh
+++ clean.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-read -r -p "Are you sure you want to delete the build directory? [y/N] " response
+read -r -p "Are you sure you want to delete the build artifacts? [y/N] " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
     echo "Deleting build artifacts"
-    rm -rf build
+    rm -rf build install
     echo "Done"
 else
     echo "Nothing deleted"
--- package.sh
+++ package.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
-rm -rf /tmp/install /tmp/swift.tar.gz
-./swift/utils/build-script --preset=buildbot_linux_1404 install_destdir=/tmp/install installable_package=/tmp/swift.tar.gz
+#!/bin/bash
+pushd `dirname $0` > /dev/null
+WHERE_I_AM=`pwd`
+popd > /dev/null
+INSTALL_DIR$=${WHERE_I_AM}/install
+PACKAGE=${WHERE_I_AM}/swift.tar.gz
+rm -rf $INSTALL_DIR $PACKAGE
+./swift/utils/build-script --preset=buildbot_linux_1404 install_destdir=${INSTALL_DIR} installable_package=${PACKAGE}
