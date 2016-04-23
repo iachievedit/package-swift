9565f84237d355222671490ee17c9e27b5dcfc35
Joseph Bell <joe@iachieved.it>
2016-04-23 13:37:17 +0000

    ARMv7-specific compilation routines

--- get.sh
+++ get.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 git clone https://github.com/apple/swift.git swift
-cd swift && ./utils/update-checkout --clone
+./swift/utils/update-checkout --clone
+ARCH=`arch`
+if [[ $ARCH =~ armv7 ]]; then
+  echo "+ Building for ARM, checkout hpux735 LLVM"
+  rm -rf llvm
+  git clone --branch arm https://github.com/hpux735/swift-llvm llvm 
+fi
--- package.sh
+++ package.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 pushd `dirname $0` > /dev/null
 WHERE_I_AM=`pwd`
+ARCH=`arch`
 popd > /dev/null
 INSTALL_DIR=${WHERE_I_AM}/install
 PACKAGE=${WHERE_I_AM}/swift.tar.gz
 LSB_RELEASE=`lsb_release -rs  | tr -d .`
 rm -rf $INSTALL_DIR $PACKAGE
+if [[ $ARCH =~ armv7 ]]; then
+echo "+ Building for ARM"
+./swift/utils/build-script --preset=buildbot_linux_armv7 install_destdir=${INSTALL_DIR} installable_package=${PACKAGE}
+else
+echo "+ Building for x86"
 ./swift/utils/build-script --preset=buildbot_linux_${LSB_RELEASE} install_destdir=${INSTALL_DIR} installable_package=${PACKAGE}
+fi
