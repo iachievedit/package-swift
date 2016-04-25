1235259bc21b44619df2554255d38fcdd5dfb232
Joe <joe@iachieved.it>
2016-04-25 08:38:18 -0500

    Swift 3.0

--- swiftrevs.swift
+++ swiftrevs.swift
@@ -3,18 +3,18 @@ import Glibc
 extension String {
   subscript (r: Range<Int>) -> String {
     get {
-      let startIndex = self.startIndex.advancedBy(r.startIndex)
-      let endIndex   = self.startIndex.advancedBy(r.endIndex)
+      let startIndex = self.startIndex.advanced(by:r.startIndex)
+      let endIndex   = self.startIndex.advanced(by:r.endIndex)
             
-      return self[Range(start: startIndex, end: endIndex)]
+      return self[Range(startIndex..<endIndex)]
     }
   }
 }
 
-func getGitRevision(dirname:String) -> String {
+func getGitRevision(directory:String) -> String {
   let BUFSIZE = 128
-  let cwd     = String.fromCString(getcwd(nil, 0))!
-  let rc      = chdir(dirname)
+  let cwd     = String(cString:getcwd(nil, 0))
+  let rc      = chdir(directory)
   
   guard rc == 0 else {
     return "ERROR"
@@ -22,9 +22,9 @@ func getGitRevision(dirname:String) -> String {
   
   var rev  = ""
   let pipe = popen("/usr/bin/git rev-parse HEAD", "r")
-  var buf  = [CChar](count:BUFSIZE, repeatedValue:CChar(0))
+  var buf  = [CChar](repeating:CChar(0), count:BUFSIZE)
   while fgets(&buf, Int32(BUFSIZE), pipe) != nil {
-    rev = String.fromCString(buf)!
+    rev = String(cString:buf)
   }
  rev = rev[0...9]
 
@@ -33,9 +33,9 @@ func getGitRevision(dirname:String) -> String {
   return rev
 }
 
-let dirs = ["swift", "llvm", "clang", "lldb", "cmark", "llbuild", "swiftpm", "swift-corelibs-xctest", "swift-corelibs-foundation", "swift-integration-tests"]
+let dirs = ["swift", "llvm", "clang", "lldb", "compiler-rt", "cmark", "llbuild", "swiftpm", "swift-corelibs-xctest", "swift-corelibs-foundation", "swift-integration-tests", "swift-corelibs-libdispatch"]
 
 for dir in dirs {
-  let rev = getGitRevision(dir)
+  let rev = getGitRevision(directory:dir)
   print("\(dir):\(rev)")
 }
