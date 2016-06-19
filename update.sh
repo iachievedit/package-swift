11eabf0ddca302a93b64697af0c00510e7bbf51b
Joe <joe@iachieved.it>
2016-06-19 15:17:41 +0000

    Workaround for update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+ARCH=`arch`
+if [[ $ARCH =~ arm ]]; then
+  echo "+ Building for ARM, remove hpux735 LLVM"
+  rm -rf llvm
+fi
 ./swift/utils/update-checkout
+if [[ $ARCH =~ arm ]]; then
+  echo "+ Building for ARM, checkout hpux735 LLVM"
+  rm -rf llvm
+  git clone --branch arm https://github.com/hpux735/swift-llvm llvm
+fi
