726b0ba51963e23b72ad7cc8bb158f9935fac402
Joe <joe@iachieved.it>
2016-06-24 23:47:43 -0500

    Updated swiftrevs to latest 3.0

--- swiftrevs.swift
+++ swiftrevs.swift
@@ -11,8 +11,19 @@ extension String {
   }
 }
 
-func getGitRevision(directory:String) -> String {
+func exec(_ command:String) -> String {
   let BUFSIZE = 128
+  let pipe = popen(command, "r")
+  var buf  = [CChar](repeating:CChar(0), count:BUFSIZE)
+  var output:String = ""
+  while fgets(&buf, Int32(BUFSIZE), pipe) != nil {
+    output = String(cString:buf)
+  }
+  return output
+}
+
+func gitRevision(_ directory:String) -> String {
+
   let cwd     = String(cString:getcwd(nil, 0))
   let rc      = chdir(directory)
   
@@ -20,22 +31,48 @@ func getGitRevision(directory:String) -> String {
     return "ERROR"
   }
   
-  var rev  = ""
-  let pipe = popen("/usr/bin/git rev-parse HEAD", "r")
-  var buf  = [CChar](repeating:CChar(0), count:BUFSIZE)
-  while fgets(&buf, Int32(BUFSIZE), pipe) != nil {
-    rev = String(cString:buf)
-  }
- rev = rev[0...9]
+  var rev = exec("/usr/bin/git rev-parse HEAD")
+  rev = rev[0...9]
 
   chdir(cwd)
 
   return rev
 }
 
+func gitFetchURL(_ dirname:String) -> String {
+  let cwd     = String(cString:getcwd(nil, 0))
+  let rc      = chdir(dirname)
+  
+  guard rc == 0 else {
+    return "ERROR"
+  }
+  
+  let url = exec("/usr/bin/git remote show origin -n|grep Fetch| echo -n `cut --characters=14-`")
+  
+  chdir(cwd)
+  
+  return url
+}
+
+func gitBranch(_ dirname:String) -> String {
+  let cwd     = String(cString:getcwd(nil, 0))
+  let rc      = chdir(dirname)
+
+  guard rc == 0 else {
+    return "ERROR"
+  }
+
+  let url  = exec("/usr/bin/git branch | echo -n `cut --characters=2-`")
+  chdir(cwd)
+
+  return url
+}
+
 let dirs = ["swift", "llvm", "clang", "lldb", "compiler-rt", "cmark", "llbuild", "swiftpm", "swift-corelibs-xctest", "swift-corelibs-foundation", "swift-integration-tests", "swift-corelibs-libdispatch"]
 
 for dir in dirs {
-  let rev = getGitRevision(directory:dir)
-  print("\(dir):\(rev)")
+  let fetch  = gitFetchURL(dir)
+  let rev    = gitRevision(dir)
+  let branch = gitBranch(dir)
+  print("\(dir),\(fetch),\(branch),\(rev)")
 }
