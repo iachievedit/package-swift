e32b2f9909141c7cba5478a1b5d6794e7089a195
Brian Gesiak <modocache@gmail.com>
2016-07-22 15:35:40 -0400

    [android] --android option gets libicu, NDK
    Begin adapting the scripts to perform a Swift Android build. The Android build
    requires libicu and the Android NDK.

--- get.sh
+++ get.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+set -e
+set -x
+
 git clone https://github.com/apple/swift.git swift
 ./swift/utils/update-checkout --clone
 ARCH=`arch`
@@ -6,4 +10,13 @@ if [[ $ARCH =~ arm ]]; then
   echo "+ Building for ARM, checkout hpux735 LLVM"
   rm -rf llvm
   git clone --branch arm https://github.com/hpux735/swift-llvm llvm 
+fi
+
+if [[ "$1" == "--android" ]]; then
+  # The Android build depends on libicu.
+  git clone https://github.com/SwiftAndroid/libiconv-libicu-android.git
+  # Download a copy of the latest supported Android NDK.
+  wget http://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip
+  unzip android-ndk-r12b-linux-x86_64.zip
+  rm android-ndk-r12b-linux-x86_64.zip
 fi
