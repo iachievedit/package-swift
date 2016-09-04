e438997f2bc2b7f443e2a7b45813b33fa80cfbb8
Joseph Bell <joe@iachieved.it>
2016-09-04 12:59:43 +0000

    Fixups for building Swift 3.0 on ARM

--- package.sh
+++ package.sh
@@ -9,7 +9,8 @@ LSB_RELEASE=`lsb_release -rs  | tr -d .`
 rm -rf $INSTALL_DIR $PACKAGE
 if [[ $ARCH =~ armv7 ]]; then
 echo "+ Building for ARM"
-./swift/utils/build-script --preset=buildbot_linux_armv7 install_destdir=${INSTALL_DIR} installable_package=${PACKAGE}
+./swift/utils/build-script --build-subdir buildbot_linux -R --lldb --llbuild --xctest --swiftpm --foundation --libdispatch -- --install-libdispatch --install-foundation --install-swift --install-lldb --install-llbuild --install-xctest --install-swiftpm --install-prefix=/usr '--swift-install-components=autolink-driver;compiler;clang-builtin-headers;stdlib;swift-remote-mirror;sdk-overlay;dev' --build-swift-static-stdlib --build-swift-static-sdk-overlay --skip-test-lldb --install-destdir=${INSTALL_DIR} --installable-package=${PACKAGE}
+#./swift/utils/build-script --preset=buildbot_linux_armv7 install_destdir=${INSTALL_DIR} installable_package=${PACKAGE}
 else
 echo "+ Building for x86"
 ./swift/utils/build-script --preset=buildbot_linux_${LSB_RELEASE} install_destdir=${INSTALL_DIR} installable_package=${PACKAGE}
--- post-install.sh
+++ post-install.sh
@@ -1,16 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 #
 # 
-cp -R swift-corelibs-libdispatch/dispatch/ install/usr/lib/swift
+pushd `dirname $0` > /dev/null
+WHERE_I_AM=`pwd`
+ARCH=`arch`
+popd > /dev/null
+INSTALL_DIR=${WHERE_I_AM}/install
+PACKAGE=${WHERE_I_AM}/swift.tar.gz
 
-cp ./build/buildbot_linux/libdispatch-linux-armv7/src/swift/Dispatch.swiftdoc install/usr/lib/swift/linux/armv7/
-cp ./build/buildbot_linux/libdispatch-linux-armv7/src/swift/Dispatch.swiftmodule install/usr/lib/swift/linux/armv7/
-cp ./build/buildbot_linux/libdispatch-linux-armv7/src/libdispatch.la install/usr/lib/swift/linux/
-cp ./build/buildbot_linux/libdispatch-linux-armv7/src/.libs/libdispatch.so install/usr/lib/swift/linux
+cp -R swift-corelibs-libdispatch/dispatch/ ${INSTALL_DIR}/usr/lib/swift
 
-mkdir -p install/usr/lib/swift/os
-cp swift-corelibs-libdispatch/os/linux_base.h install/usr/lib/swift/os
-cp swift-corelibs-libdispatch/os/object.h install/usr/lib/swift/os
+cp ./build/buildbot_linux/libdispatch-linux-armv7/src/swift/Dispatch.swiftdoc ${INSTALL_DIR}/usr/lib/swift/linux/armv7/
+cp ./build/buildbot_linux/libdispatch-linux-armv7/src/swift/Dispatch.swiftmodule ${INSTALL_DIR}/usr/lib/swift/linux/armv7/
+cp ./build/buildbot_linux/libdispatch-linux-armv7/src/libdispatch.la ${INSTALL_DIR}/usr/lib/swift/linux/
+cp ./build/buildbot_linux/libdispatch-linux-armv7/src/.libs/libdispatch.so ${INSTALL_DIR}/usr/lib/swift/linux
+
+mkdir -p ${INSTALL_DIR}/usr/lib/swift/os
+cp swift-corelibs-libdispatch/os/linux_base.h ${INSTALL_DIR}/usr/lib/swift/os
+cp swift-corelibs-libdispatch/os/object.h ${INSTALL_DIR}/usr/lib/swift/os
 
 # Retar
-tar -C install -czvf swift.tar.gz .
+tar -C ${INSTALL_DIR} -czvf ${PACKAGE} .
