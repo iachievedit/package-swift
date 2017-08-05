954ab2eeb8c1572c26811634021a70818401bb0a
Joe <joe@iachieved.it>
2017-08-05 17:07:13 -0500

    Swift 4.0

--- get.sh
+++ get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 2016 iAchieved.it LLC
 # MIT License (https://opensource.org/licenses/MIT)
-BRANCH=swift-3.0-branch
+BRANCH=swift-4.0-branch
 git clone https://github.com/apple/swift.git -b $BRANCH swift
 ARCH=`arch`
 if [[ $ARCH =~ arm ]]; then
@@ -9,5 +9,5 @@ if [[ $ARCH =~ arm ]]; then
   ./swift/utils/update-checkout --branch $BRANCH --clone --skip-repository llvm
   git clone --branch arm https://github.com/hpux735/swift-llvm llvm 
 else
-  ./swift/utils/update-checkout --clone --branch $BRANCH
+  ./swift/utils/update-checkout --clone #--branch $BRANCH
 fi
